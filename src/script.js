7e58bae383706b8d339c64bfce16e79796083100
Florian <florianmtddev@gmail.com>
2025-09-20 14:38:08 +0200

    Added gui button to add more sphere

--- src/script.js
+++ src/script.js
@@ -9,6 +9,19 @@ import { materialAO, metalness, roughness } from 'three/tsl'
  * Debug
  */
 const gui = new GUI()
+const debugObject = {}
+
+debugObject.createSphere = () =>
+{
+    createSphere(
+        Math.random() * 0.5,
+        { 
+            x: (Math.random() * - 0.5) * 3, 
+            y: 3, 
+            z: (Math.random() * - 0.5) * 3,
+        })
+}
+gui.add(debugObject, 'createSphere')
 
 /**
  * Base
